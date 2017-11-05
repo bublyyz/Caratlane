e10f1027c93b946b3e80cb739cacca6fc2751c48
bublyyz <31184516+bublyyz@users.noreply.github.com>
2017-11-06 00:28:56 +0530

    2nd push

--- src/com/caratlane/page/HomePage.java
+++ src/com/caratlane/page/HomePage.java
@@ -1,10 +1,32 @@
 package com.caratlane.page;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 import generic.BasePage;
 
 public class HomePage extends BasePage{
 	
-	//@FindBy(xpath="")
+	private WebDriver driver;
+	
+	@FindBy(xpath="//a/span[.='GOLD JEWELLERY']")
+	private WebElement GJmenu;
+	
+	
+	public HomePage(WebDriver driver){
+		PageFactory.initElements(driver, this);
+		this.driver=driver;
+	}
+	
+	public void hoverGJmenu(){
+		Actions actions=new Actions(driver);
+		actions.moveToElement(GJmenu).perform();				
+	}
+	
+}
 	
 
-}
+
